d795791f870a1d03c7118e39b4078603cca91c7d
Daryl Fung <darylfung96@gmail.com>
2020-04-01 23:01:11 -0500

    fix gan

--- gan.py
+++ gan.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import datasets
+import torchvision.transforms as transforms
+
 from torch.autograd import Variable
 import torch.optim as optim
-from tensorflow.examples.tutorials.mnist import input_data
-import numpy as np
 
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
@@ -15,7 +16,7 @@ import os
 
 """
 
-BATCH_SIZE = 32
+BATCH_SIZE = 512
 Z_DIM = 100
 X_SIZE = 784
 
@@ -24,14 +25,22 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         self.output_size = output_size
 
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, hidden_size*4)
-        self.fc3 = nn.Linear(hidden_size*4, output_size)
+        self.model = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.BatchNorm1d(hidden_size, 0.8),
+            nn.LeakyReLU(0.2),
+
+            nn.Linear(hidden_size, hidden_size*4),
+            nn.BatchNorm1d(hidden_size*4, 0.8),
+            nn.LeakyReLU(0.2),
+
+            nn.Linear(hidden_size*4, output_size),
+            nn.Tanh()
+        )
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return torch.tanh(self.fc3(x))
+        return self.model(x)
+
 
 class Discriminator(nn.Module):
     def __init__(self, input_size, hidden_size):
@@ -42,78 +51,88 @@ class Discriminator(nn.Module):
         self.fc3 = nn.Linear(hidden_size, 1)
 
     def forward(self, x):
-        x = F.dropout(F.relu(self.fc1(x)), 0.5)
-        x = F.dropout(F.relu(self.fc2(x)), 0.5)
+        x = F.leaky_relu(self.fc1(x), negative_slope=0.2)
+        x = F.leaky_relu(self.fc2(x), negative_slope=0.2)
         return torch.sigmoid(self.fc3(x))
 
 
-def sample_z():
-    return torch.rand(BATCH_SIZE, Z_DIM)
-
-mnist = input_data.read_data_sets('../../MNIST_data', one_hot=True)
 
 generator = Generator(Z_DIM, 256, X_SIZE)
 discriminator = Discriminator(X_SIZE, 256)
 
-d_optim = optim.Adam(discriminator.parameters(), lr=0.0001)
-g_optim = optim.Adam(generator.parameters(), lr=0.0001)
+d_optim = optim.Adam(discriminator.parameters(), lr=0.0002, betas=[0.5, 0.999])
+g_optim = optim.Adam(generator.parameters(), lr=0.0002, betas=[0.5, 0.999])
 
 
 picture_out_count = 0
 BCELoss = nn.BCELoss()
 
-for iteration in range(20000):
+dataloader = torch.utils.data.DataLoader(
+    datasets.MNIST(
+        "./data/mnist",
+        train=True,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
+        ),
+    ),
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+    drop_last=True
+)
 
-    d_optim.zero_grad()
+for epoch in range(200):
+    for i, (imgs, _) in enumerate(dataloader):
+        d_optim.zero_grad()
 
-    x_, _ = mnist.train.next_batch(BATCH_SIZE)
-    x_ = Variable(torch.from_numpy(x_))
+        x_ = imgs.view(BATCH_SIZE, -1)
 
-    #pass input through discriminator
-    d_real_value = discriminator(x_)
-    d_real_error = BCELoss(d_real_value, Variable(torch.ones(BATCH_SIZE)))
+        # train generator
+        g_optim.zero_grad()
 
-    #pass fake through discriminator
-    z_ = Variable(torch.rand(BATCH_SIZE, Z_DIM))
-    g_ = generator(z_)
-    d_fake_value = discriminator(g_)
-    d_fake_error = BCELoss(d_fake_value, Variable(torch.zeros(BATCH_SIZE)))
+        g_ = generator(Variable(torch.rand(BATCH_SIZE, Z_DIM)))
+        g_value = discriminator(g_)
+        g_error = BCELoss(g_value, Variable(torch.ones(BATCH_SIZE) * 0.9, requires_grad=False).unsqueeze(1))
 
-    d_total_error = d_real_error + d_fake_error
-    d_total_error.backward()
-    d_optim.step()
-
-    #train generator
-    g_optim.zero_grad()
-
-    g_ = generator(Variable(torch.rand(BATCH_SIZE, Z_DIM)))
-    g_value = discriminator(g_)
-    g_error = BCELoss(g_value, Variable(torch.ones(BATCH_SIZE)))
-
-    g_error.backward()
-    g_optim.step()
+        g_error.backward()
+        g_optim.step()
 
 
-    if iteration % 10 == 0:
-        samples = g_[:16].data.numpy() # get 16 pictures
-        fig = plt.figure(figsize=(4, 4))
-        gs = gridspec.GridSpec(4, 4)
-        gs.update(wspace=0.05, hspace=0.05)
+        #pass input through discriminator
+        d_real_value = discriminator(x_)
+        d_real_error = BCELoss(d_real_value, Variable(torch.ones(BATCH_SIZE) * 0.9, requires_grad=False).unsqueeze(1))
 
-        for i, sample in enumerate(samples):
-            ax = plt.subplot(gs[i])
-            plt.axis('off')
-            ax.set_xticklabels([])
-            ax.set_yticklabels([])
-            ax.set_aspect('equal')
-            plt.imshow(sample.reshape(28, 28), cmap='Greys_r')
+        #pass fake through discriminator
+        d_fake_value = discriminator(g_.detach())
+        d_fake_error = BCELoss(d_fake_value, Variable(torch.zeros(BATCH_SIZE), requires_grad=False).unsqueeze(1))
 
-        if not os.path.exists('out/'):
-            os.makedirs('out/')
+        d_total_error = (d_real_error + d_fake_error)/2
+        d_total_error.backward()
+        d_optim.step()
 
-        plt.savefig('out/{}.png'.format(str(picture_out_count).zfill(3)))
-        picture_out_count+=1
-        plt.close(fig)
+
+        if i % 100 == 0:
+            samples = g_[:16].data.numpy() # get 16 pictures
+            fig = plt.figure(figsize=(4, 4))
+            gs = gridspec.GridSpec(4, 4)
+            gs.update(wspace=0.05, hspace=0.05)
+
+            for i, sample in enumerate(samples):
+                ax = plt.subplot(gs[i])
+                plt.axis('off')
+                ax.set_xticklabels([])
+                ax.set_yticklabels([])
+                ax.set_aspect('equal')
+                plt.imshow(sample.reshape(28, 28), cmap='Greys_r')
+
+            if not os.path.exists('out/'):
+                os.makedirs('out/')
+
+            plt.savefig('out/{}.png'.format(str(picture_out_count).zfill(3)))
+            picture_out_count+=1
+            plt.close(fig)
+
+            print(f'epoch: {epoch} d_loss: {d_total_error} g_loss: {g_error}')
 
 
 
